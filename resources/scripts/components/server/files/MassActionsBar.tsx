76eb09ab15aa09ef7e62ca287f0da5ce5745d2af
helpy4 <106977582+HELPY4@users.noreply.github.com>
2024-06-22 17:29:18 +0500

    Update MassActionsBar.tsx

--- resources/scripts/components/server/files/MassActionsBar.tsx
+++ resources/scripts/components/server/files/MassActionsBar.tsx
@@ -67,9 +67,10 @@ const MassActionsBar = () => {
                     {loadingMessage}
                 </SpinnerOverlay>
                 <Dialog.Confirm
-                    title={'Delete Files'}
+                    {/*По умолчанию Delete Files (см. репозиторий Jexactyl)*/}
+                    title={'Удаление файлов'}
                     open={showConfirm}
-                    confirm={'Delete'}
+                    confirm={'Удалить'}
                     onClose={() => setShowConfirm(false)}
                     onConfirmed={onClickConfirmDeletion}
                 >
@@ -96,10 +97,10 @@ const MassActionsBar = () => {
                     <div className={'fixed bottom-0 mb-6 flex justify-center w-full z-50'}>
                         <Fade timeout={75} in={selectedFiles.length > 0} unmountOnExit>
                             <div css={tw`flex items-center space-x-4 pointer-events-auto rounded p-4 bg-black/50`}>
-                                <Button onClick={() => setShowMove(true)}>Move</Button>
-                                <Button onClick={onClickCompress}>Archive</Button>
+                                <Button onClick={() => setShowMove(true)}>Переместить</Button>
+                                <Button onClick={onClickCompress}>В архив</Button>
                                 <Button.Danger variant={Button.Variants.Secondary} onClick={() => setShowConfirm(true)}>
-                                    Delete
+                                    Удалить
                                 </Button.Danger>
                             </div>
                         </Fade>
