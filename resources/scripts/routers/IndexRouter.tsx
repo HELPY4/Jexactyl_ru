0eccf166216ab152af7ef7105547ff74a7312d90
helpy4 <106977582+HELPY4@users.noreply.github.com>
2024-06-23 19:50:23 +0500

    Update IndexRouter.tsx

--- resources/scripts/routers/IndexRouter.tsx
+++ resources/scripts/routers/IndexRouter.tsx
@@ -17,7 +17,6 @@ export default () => {
     const authenticated = useStoreState((state) => state.user?.data);
     const approved = useStoreState((state) => state.user.data?.approved);
     const store = useStoreState((state) => state.storefront.data!.enabled);
-    const announcements = useStoreState((state) => state.settings.data!.announcements);
     const tickets = useStoreState((state) => state.settings.data!.tickets);
     const approvals = useStoreState((state) => state.settings.data!.approvals);
 
